a7328468fe87ffe49c1d09eb2b711c92f6953f3b
baffolobill <baffolobill@yandex.ru>
2015-06-20 16:45:08 +0300

    Simply implement skipIfCustomUser if not found

--- provider/compat/__init__.py
+++ provider/compat/__init__.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from unittest import skipIf
+
+from django.conf import settings
+
+
 try:
     from django.contrib.auth.tests.utils import skipIfCustomUser
 except ImportError:
     def skipIfCustomUser(wrapped):
-        return wrapped
+        return skipIf(settings.AUTH_USER_MODEL != 'auth.User', 'Custom user model in use')(wrapped)
